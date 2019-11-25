58e1e466b34edc7b50972369ca937ba10b591a83
Ers28203006 <ersyn_sultanbaev@mail.ru>
2019-11-26 00:25:03 +0600

    Добавьте файлы проекта.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,28 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    public class StudentsController <T> : IAdding<T>, IDisplay
+    {
+        List<T> models;
+        public StudentsController()
+        {
+            models = new List<T>();
+        }
+
+        public void Add(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string  Surname{ get; set; }
+        public int Mark { get; set; }
+    }
+}
